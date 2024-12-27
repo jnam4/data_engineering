8161861d75d27b7535b8b324bd6aa943f0ec74b2
jnam4 <jnam4@stevens.edu>
2024-12-27 16:38:57 +0000

    upload

---
+++ codeacademy/Data Engineering/Python/2. Control Flow/Sal's Shipping.py
@@ -0,0 +1,31 @@
+#Project:   Sal's Shipping
+#Date:      2024.12.27
+#Creator:   Jiyun Nam
+
+weight = int(input("How weight it is?"))
+
+#Ground shipping
+if weight <= 2:
+  costs=weight*1.50+0.00
+elif weight >= 2 and weight <= 6:
+  costs=weight*3.00+0.00
+elif weight >=6 and weight <= 10:
+  costs=weight*4.00+0.00
+else:
+  costs=weight*4.75+0.00
+print("Cost of the ground shipping is " + str(costs))
+
+#Ground Shipping Premium
+costs_P=125 #Flat rate
+print("Cost of the ground shipping premium is "+str(costs_P))
+
+#Ground Shipping Premium
+if weight <= 2:
+  Dcosts=weight*4.50
+elif weight >=2 and weight <=6:
+  Dcosts=weight*9.00
+elif weight >=6 and weight <=10:
+  Dcosts=weight*12.00
+else:
+  Dcosts=weight*14.25
+print("Cost of the Drone shipping  is " + str(Dcosts))
